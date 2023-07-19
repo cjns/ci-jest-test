c5d64d43dcfd8a446208491111894ac60408cc8f
cjns <67065839+cjns@users.noreply.github.com>
2023-07-19 23:46:08 +0100

    Add jsdom environment

--- scripts/tests/calc.test.js
+++ scripts/tests/calc.test.js
@@ -6,7 +6,10 @@ describe("Calculator", () => {                    //  Top level parent descripti
   describe("Addition function", () => {           //  Second level description.
     test("Should return 42 for 20 + 22", () => {  //  Test method.
       expect(addition(20, 22)).toBe(42);          //  Expect method.
-    })
+    });
+    test("Should return 73 for 42 + 31", () => {  //  Test method.
+      expect(addition(42, 31)).toBe(73);          //  Expect method.
+    });
   });
   describe("Subtraction function", () => {        //  Second level description.
 
