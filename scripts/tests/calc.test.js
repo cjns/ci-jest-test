6b31dab9af90ec88107b276f93b5029c44943446
cjns <67065839+cjns@users.noreply.github.com>
2023-07-19 22:39:50 +0100

    Initial Commit

---
+++ scripts/tests/calc.test.js
@@ -0,0 +1,20 @@
+// Import the function to be tested.
+const addition = require("../calc");
+
+// Add the test code.
+describe("Calculator", () => {                    //  Top level parent description.
+  describe("Addition function", () => {           //  Second level description.
+    test("Should return 42 for 20 + 22", () => {  //  Test method.
+      expect(addition(20, 22)).toBe(42);          //  Expect method.
+    })
+  });
+  describe("Subtraction function", () => {        //  Second level description.
+
+  });
+  describe("Multiply function", () => {           //  Second level description.
+
+  });
+  describe("Division function", () => {           //  Second level description.
+
+  });
+})
